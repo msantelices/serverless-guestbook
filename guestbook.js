c16f2bc15f5d22817b1f67c6dfd37241f488a667
Mauricio Santelices <32970238+msantelices@users.noreply.github.com>
2022-06-11 19:09:37 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://82ce4fdc.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
